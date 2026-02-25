34136e7afa881efd8be888a29c7d3f126e159f4c
Maria Paula Fonseca <paulablr1112@gmail.com>
2026-02-24 21:47:33 -0500

    Add files via upload

--- Corte1/profejulio/App.java
+++ Corte1/profejulio/App.java
@@ -5,7 +5,7 @@ public class App {
     {
         Numero numero1 = new Numero(5);
         Numero numero2 = new Numero(7);
-        Numero suma = new Numero();
+        Numero multiplicacion = new Numero();
         Scanner teclado = new Scanner(System.in);
         Vista vista = new Vista();
         vista.mostrarTitulo();
@@ -14,14 +14,17 @@ public class App {
         int n1 = teclado.nextInt();//si es entero se pede guardar en n1, asegura
         numero1.setNumero(n1);
         vista.mostrarNumero();
-        System.out.println("Digite numero 2");
         int n2 = teclado.nextInt();
         numero2.setNumero(n2);
+        vista.mostrarNumero2();
 
-        int s = numero1.getNumero() + numero2.getNumero(); 
-        suma.setNumero(s);
 
-        System.out.println(suma.getNumero());
+        numero2.getNumero(); 
+
+        int m = numero1.getNumero() * numero2.getNumero(); 
+        multiplicacion.setNumero(m);
+
+        System.out.println(multiplicacion.getNumero());
         teclado.close();
     }
 }
--- Corte1/profejulio/Vista.java
+++ Corte1/profejulio/Vista.java
@@ -2,12 +2,17 @@ public class Vista {
     public Vista(){
     }
     public void mostrarTitulo(){
-        System.out.println("Programa para sumar numero");
+        System.out.println("Programa para multiplicar numeros");
 
     }
     public void mostrarNumero(){
         System.out.println("Digite numero");
     }
+    public void mostrarNumero2(){
+        System.out.println("Digite segundo numero");
+    }
+
+
     
     
 }
