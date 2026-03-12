5f5cb9b078618feafe5b2a5eec251f72f4d0f6a2
Maria Paula Fonseca <paulablr1112@gmail.com>
2026-03-12 16:08:33 -0500

    Update StatsDemo.java

--- Corte1/Java/MetodoEstatico/StatsDemo.java
+++ Corte1/Java/MetodoEstatico/StatsDemo.java
@@ -1,17 +1,18 @@
 public class StatsDemo {
-static class Stats {
-public static int sum(int[] a) {
+static class Stats {// clase 
+public static int sum(int[] a) {//metodo
 int total = 0;
-for (int x : a) total += x; // enhanced for-loop
+for (int x : a) total += x; // enhanced for-loop. 
 return total;
 }
 public static double average(int[] a) {
-return (double) sum(a) / a.length;
+return (double) sum(a) / a.length;// obtener el numero de elementos que tiene un arreglo
 }
 }
 public static void main(String[] args) {
 int[] grades = {80, 75, 90, 60, 95};
-System.out.println("Sum: " + Stats.sum(grades));
-System.out.println("Average: " + Stats.average(grades));
+System.out.println("Sum: " + Stats.sum(grades));//llama al metodo estatico sum
+System.out.println("Average: " + Stats.average(grades));// llama al metodo estatico average(promedio)
 }
 }
+
