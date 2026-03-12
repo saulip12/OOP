238b9b91401c8ec5d28738d0fd8feda159908e14
Maria Paula Fonseca <paulablr1112@gmail.com>
2026-03-12 16:11:37 -0500

    Update StatsDemo.java

--- Corte1/Java/MetodoEstatico/StatsDemo.java
+++ Corte1/Java/MetodoEstatico/StatsDemo.java
@@ -11,8 +11,9 @@ return (double) sum(a) / a.length;// obtener el numero de elementos que tiene un
 }
 public static void main(String[] args) {
 int[] grades = {80, 75, 90, 60, 95};
-System.out.println("Sum: " + Stats.sum(grades));//llama al metodo estatico sum
+System.out.println("Sum: " + Stats.sum(grades));//busca la clase stats, llama al metodo suma, argumento arrays
 System.out.println("Average: " + Stats.average(grades));// llama al metodo estatico average(promedio)
 }
 }
+
 
